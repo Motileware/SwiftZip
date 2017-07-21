1941665f5e39639d8172a52646283672bda2d41f
weyhan <weyhan@motileware.com>
2017-07-22 00:33:56 +0800

    Initial Commit

---
+++ SwiftZip/SwiftZip.h
@@ -0,0 +1,19 @@
+//
+//  SwiftZip.h
+//  SwiftZip
+//
+//  Created by WeyHan Ng on 22/07/2017.
+//  Copyright © 2017 Motileware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftZip.
+FOUNDATION_EXPORT double SwiftZipVersionNumber;
+
+//! Project version string for SwiftZip.
+FOUNDATION_EXPORT const unsigned char SwiftZipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftZip/PublicHeader.h>
+
+
